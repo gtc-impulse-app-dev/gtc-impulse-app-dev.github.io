e50c63af801cb96170ad5727c6f05e9a77ef2648
Rishi Varma Kalidindi <rishivarma.gtc@gmail.com>
2025-06-27 06:34:47 -0400

    second commit

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 // Register service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
         // navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-        navigator.serviceWorker.register('/gtc-impulse-app-dev/service-worker.js').then((registration) => {
             console.log('Service Worker registered with scope:', registration.scope);
         }, (err) => {
             console.error('Service Worker registration failed:', err);
